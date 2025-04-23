be93cc46da4174d81c9ed7f1db9aa30f45a3605f
Joseph Perthold <Bedlesssgod@gmail.com>
2025-04-23 16:30:45 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,22 +4,34 @@ import http from "http";
 
 const app = express();
 const server = http.createServer(app);
-const session =  Date.now() + ".log";
-
+const session = Date.now() + ".log";
 
 app.use((req, res, next) => {
-    console.log(`${Date.now()}|${req.method}|${req.host + req.url}|${req.ip}|${req.protocol}|${req.get("user-agent")}`);
-    writeFile(session, `${Date.now()}|${req.method}|${req.host + req.url}|${req.ip}|${req.protocol}|${req.get("user-agent")}\n`, { flag: "a" }, (err) => {
-        if (err) {
-            console.error("Error writing log file", err);
-        }
-    });
-    next();
+  console.log(
+    `${Date.now()}|${req.method}|${req.host + req.url}|${req.ip}|${
+      req.protocol
+    }|${req.get("user-agent")}`
+  );
+  writeFile(
+    session,
+    `${Date.now()}|${req.method}|${req.host + req.url}|${req.ip}|${
+      req.protocol
+    }|${req.get("user-agent")}\n`,
+    { flag: "a" },
+    (err) => {
+      if (err) {
+        console.error("Error writing log file", err);
+      }
+    }
+  );
+  next();
 });
 
-app.use(express.static(process.env.WS_FILEPATH?? "/var/www/html"));
+app.use(express.static(process.env.WS_FILEPATH ?? "/var/www/html"));
 
-server.listen(process.env.WS_PORT?? 80, () => {
-  console.log("Server running at http://localhost:" + (process.env.WS_PORT?? 80));
+server.listen(process.env.WS_PORT ?? 80, () => {
+  console.log(
+    "Server running at http://localhost:" + (process.env.WS_PORT ?? 80)
+  );
   console.log("Session log file: " + session);
 });
